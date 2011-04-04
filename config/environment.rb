4ceefb289b6a23ec41528f250d9c408990090085
estill01 <ehs.stillman@gmail.com>
2011-04-04 15:10:07 -0400

    minor

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.gem "jammit"
 # Initialize the rails application
 BaseApp::Application.initialize!
 
-config.gem "jammit"
