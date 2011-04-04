7dd6950d319dacce1d42e143934895ef024ab755
estill01 <ehs.stillman@gmail.com>
2011-04-04 15:07:24 -0400

    [deployment] added heroku_jammit

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
+config.gem "jammit"
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BaseApp::Application.initialize!
 
-config.gem "jammit"
